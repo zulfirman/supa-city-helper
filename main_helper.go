7131d93fba3c2cd91b921832bc247b870b8347e4
Zulfirman <zulfirman098@gmail.com>
2023-05-31 13:49:16 +0700

    fix pagination wrong number

--- main_helper.go
+++ main_helper.go
@@ -316,14 +316,27 @@ func PaginateInfo(paging Pagination, totalData int64) H {
 	// Calculate the total number of pages.
 	totalPages := math.Ceil(float64(totalData) / float64(paging.Limit))
 
-	// Calculate the next and previous page numbers.
+	// Calculate the next page numbers.
 	nextPage := paging.Page + 1
-	if nextPage >= int(totalPages) {
+	if paging.Page>=1{
+		nextPage++
+	}else{
+		if paging.Page < 2 {
+			nextPage = 2
+		}
+	}
+
+	pageDisplay :=paging.Page+1
+	if pageDisplay >= int(totalPages) {
 		nextPage = 0
 	}
-	previousPage := paging.Page - 1
-	if previousPage < 1 {
-		previousPage = 0
+
+	// Calculate the next and previous page numbers.
+	previousPage := 0
+	if pageDisplay<2{
+		previousPage=0
+	}else{
+		previousPage = pageDisplay - 1
 	}
 
 	// Increment the current page number.
@@ -364,7 +377,6 @@ func ToSnakeCase(camel string) string {
 	}
 	return string(buf)
 }
-
 //end paginate helper
 
 func ParseJWT(c echo.Context) (map[string]interface{}, error) { //parse jwt token
